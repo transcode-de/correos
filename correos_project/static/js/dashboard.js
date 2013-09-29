2f51132276b2ff88486147b0ab959ea6b2bf8dd2
Max Brauer <max.brauer@inqbus.de>
2013-09-29 23:52:06 +0200

    added prev and next functionality to dashboard

--- correos_project/static/js/dashboard.js
+++ correos_project/static/js/dashboard.js
@@ -1,16 +1,30 @@
 function MainViewModel() {
   self = this;
-  self.emails = ko.observableArray([]);
-  self.fetchEmails = function() {
-    $.get('/api/email/', function(data) {
-        self.emails(_.map(data, function(email) {
+  self.emails = ko.observableArray();
+  self.next_url = ko.observable('/api/email/');
+  self.previous_url = ko.observable();
+
+  self.fetchEmails = function(url) {
+    $.get(url, function(data) {
+        console.log(data);
+        self.next_url(data['next']);
+        self.previous_url(data['previous']);
+        self.emails(_.map(data['results'], function(email) {
           email.header = $.parseJSON(email.header);
           return email;
-        }));
-      });
+      }));
+    });
   };
-  self.fetchEmails();
-  console.log(self.emails());
+
+  self.fetchNextEmails = function() {
+    self.fetchEmails(self.next_url());
+  };
+
+  self.fetchPreviousEmails = function() {
+    self.fetchEmails(self.previous_url());
+  };
+
+  self.fetchEmails(self.next_url());
 }
 
 ko.applyBindings(new MainViewModel());
