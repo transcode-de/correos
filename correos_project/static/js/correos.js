3d0aed4e216c25e1da5642013991c03716263b0a
Stephan Hoyer <ste.hoyer@gmail.com>
2013-09-29 14:39:45 +0200

    Add basic knockout view model to handle domain switch

--- correos_project/static/js/correos.js
+++ correos_project/static/js/correos.js
@@ -1,4 +1,34 @@
-$(document).ready(function() {
+function Email(data) {
+  this.header = {};
+  this.raw = '';
+  this.sender = '';
+  this.recipient = '';
+  this.domain = '';
+  this.date = new Date();
+  this.body = '';
+  this.subject = '';
+}
+
+function MainViewModel() {
+  self = this;
+
+  self.domain = ko.observable({ name: 'None' });
+  self.domains = ko.observableArray([]);
+
+  setInterval(function() {
+    //$.get('/api/domain/', function(domains) {
+      self.domains([
+        { name: 'example.net' },
+        { name: 'example.org' },
+        { name: 'example.foo' }
+      ]);
+    //});
+  }, 1000);
+}
+
+ko.applyBindings(new MainViewModel());
+
+$(function() {
 	$('.postbox-select, .email-select, .email-detail').niceScroll();
 	$('#reload-domains').tooltip({'placement': 'bottom'});
 });
